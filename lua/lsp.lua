08908e07be166ea87aa23e44154f4b89475e84c3
WizeRacoon <wizeracoon@pop-os.localdomain>
2022-11-08 15:14:52 +0100

    lualanguageserver

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -59,3 +59,22 @@
   require('lspconfig')['clangd'].setup {
     capabilities = capabilities
   }
+  
+require('lspconfig')['sumneko_lua'].setup {
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = {'vim'},
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
